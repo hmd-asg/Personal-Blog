3ec405d3e10af92cf1292026aa9d121c73f7ef7d
hamid assaghli <hamid@hamids-MBP.lan>
2024-07-05 21:48:23 -0400

    updated html css js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,12 +6,12 @@ let light = true;
 btn_toggle.addEventListener('click', () => {
     if (light) {
         document.body.style = 'background-color : #393832; color : white';
-        btn_toggle.style = "background: url('./images/light.png') no-repeat;background-size: cover;";
+        btn_toggle.style = "background: url('./assets/images/light.png') no-repeat;background-size: cover;";
         light = false;
     }
     else {
         document.body.style = 'background-color : white; color : black';
-        btn_toggle.style = "background: url('./images/dark.png') no-repeat;;background-size: cover;";
+        btn_toggle.style = "background: url('./assets/images/dark.png') no-repeat;;background-size: cover;";
         light = true;
     }
 }
